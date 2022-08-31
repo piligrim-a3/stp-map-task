34ced926136c0f0174a3771ccc1de36f8664934f
chiki-andrei <andrei.666.666@mail.ru>
2022-08-31 14:21:25 +0900

    Line split and map fill

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,31 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, Integer> logs = new HashMap<>(); //Первое значение ключ, второе значение объем данных
+        Map<Integer, Integer> logs = new HashMap<>(); //Первое значение ключ, второе значение объем данных.
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("access.log")); //Пытаемся открыть файл для считывания
-        } catch (FileNotFoundException e) {
+            BufferedReader reader = new BufferedReader(new FileReader("access.log")); //Пытаемся открыть файл для считывания.
+            String line;
+            String regex = " +"; //Фильтр для разделения строки, любое кол-во пробелов будет являться разделителем.
+
+            int key = 1;
+            while ((line = reader.readLine()) != null) {
+                key++;
+                String[] split;
+                split = line.split(regex);
+
+                //Исходя из структуры log файла, 4 элемент будет являться необходимым для нас количеством байт.
+                //Поэтому добавляем ключ - номер считываемой линии и само количество байт.
+                logs.put(key, Integer.parseInt(split[4]));
+            }
+            logs.forEach((key1, value) -> System.out.println("key:" + key1 + " value: " + value)); //Для каждого элемента имеем его количество байт
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
