c3418f1ecb902c267b713c5d9069f99ba25e20dd
chiki-andrei <andrei.666.666@mail.ru>
2022-08-31 15:34:02 +0900

    File output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,21 @@
 package com.company;
 
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Map<Integer, Integer> unsortedLogs = new HashMap<>(); //Первое значение ключ, второе значение объем данных.
+        String filename = "out.txt"; //Выходной файл.
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("access.log")); //Пытаемся открыть файл для считывания.
+            Files.createFile(Paths.get(filename)); //Создаем файл для сохранения результата
             String line;
             String regex = " +"; //Фильтр для разделения строки: любое кол-во пробелов будет являться разделителем.
 
@@ -24,7 +30,6 @@ public class Main {
                 lineNumber++;
             }
 
-
             //Сортируем данные по объему в порядке убывания.
             Map<Integer, Integer> sortedLogs = unsortedLogs.entrySet().stream()
                     .sorted(Comparator.comparingInt(e -> -e.getValue()))
@@ -32,11 +37,12 @@ public class Main {
                             (
                                     Map.Entry::getKey,
                                     Map.Entry::getValue,
-                                    (k, v) -> { throw new Error(); },
+                                    (k, v) -> {
+                                        throw new Error();
+                                    },
                                     LinkedHashMap::new
                             )
                     );
-
 
             ArrayList<Integer> bytes = new ArrayList<>(); //Массив для хранения 10 значений указывающих на номера строк с наибольших количеством байт.
             for (int i = 0; i < 10; i++) {
@@ -49,21 +55,24 @@ public class Main {
             reader = new BufferedReader(new FileReader("access.log"));
             lineNumber = 1;
 
-
-            //Пройдем по файлу еще раз, сохраняя информацию о 10-ти строках, которые имеют наибольшее количество байт.
             while ((line = reader.readLine()) != null) {
                 if (lineNumber == bytes.get(0)) {
-                    System.out.println(line);
+                    Files.writeString(Paths.get(filename), line, StandardOpenOption.APPEND); //Добавляем к файлу текущую строку.
                     bytes.remove(0);
-                }
-                if (bytes.size() == 0) {
-                    break;
+                    if (bytes.size() != 0) {
+                        Files.writeString(Paths.get(filename), "\n", StandardOpenOption.APPEND); //Переводим каретку на новую строку.
+                    } else {
+                        break;
+                    }
                 }
                 lineNumber++;
             }
 
             reader.close();
-        } catch (Exception e) {
+        } catch (FileAlreadyExistsException e) {
+            System.out.println("File with name " + filename + " already exists");
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
