8144e8e5cd7ff8fcfaa0eca9ce809dbe5e96ab14
IIA <51432649+SkrageIl@users.noreply.github.com>
2023-01-28 23:47:34 +0900

    Read file

--- task/Main.java
+++ task/Main.java
@@ -1,7 +1,16 @@
 package task;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("access.log"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
